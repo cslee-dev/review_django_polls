c818201752ce6d5693c3664e09774baeb54a9925
cslee-dev <dev.cslee@gmail.com>
2019-08-03 23:31:13 +0900

    [add] polls build And poll list

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, results, vote, detail
 
 app_name = 'polls'
 
 urlpatterns = [
-    path('', index, name='main')
+    path('', index, name='main'),
+    path('<int:question_id>}', detail, name='detail'),
+    path('<int:question_id>/results', results, name='results'),
+    path('<int:question_id>/vote', vote, name='vote')
 ]
--- index/views.py
+++ index/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Question, Choice
 # Create your views here.
 
 
 def index(req):
-    return HttpResponse('<h1>hihi</h1>')
+    question_list = Question.objects.all()
+    return render(req, 'index/index.html', {"question_list": question_list})
+
+
+def detail(req, question_id):
+    return HttpResponse('detail')
+
+
+def results(req, question_id):
+    return HttpResponse('results')
+
+
+def vote(req, question_id):
+    return HttpResponse('vote')
+
